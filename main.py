1127702472991572dbf294d70b03ad2ce8f8f198
Olivia <oliviakim217@gmail.com>
2023-08-23 22:31:16 -0400

    Separte each entry

--- main.py
+++ main.py
@@ -6,25 +6,26 @@ import time
 service = Service("/Users/olivia/Documents/IT/chromedriver")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://kyocharocalgary.com/yellowpage")
-
-for i in range(1, 5):
-    # Click on business name
-    xpath = '//*[@id="fboardlist"]/div/table/tbody/tr[' + str(i) + ']/td[2]/a'
-    business_name = driver.find_element(By.XPATH, xpath)
-    business_name.click()
-    time.sleep(1)
+for page in range(11, 12):
+    driver.get("https://kyocharocalgary.com/yellowpage?page=" + str(page))
+    print("page" + str(page))
     
-    # Print content
-    content = driver.find_element(By.ID, "bo_v_con").text
-    print(content)
-    time.sleep(1)
+    for i in range(1, 51):
+        # Click on business name
+        xpath = '//*[@id="fboardlist"]/div/table/tbody/tr[' + str(i) + ']/td[2]/a'
+        business_name = driver.find_element(By.XPATH, xpath)
+        business_name.click()
+        time.sleep(1)
+        
+        # Print content
+        content = driver.find_element(By.ID, "bo_v_con").text
+        print(content)
+        print(str(i) + "------------")
+        time.sleep(1)
 
-    # Return to previous page
-    driver.back()
-    time.sleep(1)
+        # Return to previous page
+        driver.back()
+        time.sleep(1)
 
 
 # driver.close()
-
-
