894c2ab690c6c879f12c49dd4517c87b2a35ce34
Olivia <oliviakim217@gmail.com>
2023-08-23 21:10:47 -0400

    Automate link clicks for all contacts

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 
 service = Service("/Users/olivia/Documents/IT/chromedriver")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://www.amazon.com")
+driver.get("https://kyocharocalgary.com/yellowpage")
+
+for i in range(1, 5):
+    # Click on business name
+    xpath = '//*[@id="fboardlist"]/div/table/tbody/tr[' + str(i) + ']/td[2]/a'
+    business_name = driver.find_element(By.XPATH, xpath)
+    business_name.click()
+    time.sleep(1)
+    
+    # Print content
+    content = driver.find_element(By.ID, "bo_v_con").text
+    print(content)
+    time.sleep(1)
+
+    # Return to previous page
+    driver.back()
+    time.sleep(1)
+
+
+# driver.close()
+
+
